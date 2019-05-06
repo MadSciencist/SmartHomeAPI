ecd7a728b9c0334dd2693a4245dadcd111d61db9
Mateusz Kryszczak <mkrysz1337@gmail.com>
2019-05-06 21:25:08 +0200

    nodeCommands endpoint

--- src/SmartHome.Core/Persistence/AppDbContext.cs
+++ src/SmartHome.Core/Persistence/AppDbContext.cs
@@ -19,8 +19,15 @@ namespace SmartHome.Core.Persistence
         public DbSet<RestTemplate> RestTemplates  { get; set; }
         public DbSet<RestTemplateValues> RestTemplateValues { get; set; }
 
+
+
         public DbSet<Dictionary> Dictionaries { get; set; }
         public DbSet<DictionaryValue> DictionaryValues { get; set; }
+
+        // Navigation properties
+        public DbSet<AppUserNodeLink> AppUserNodeLink { get; set; }
+        public DbSet<NodeCommandNodeLink> NodeCommandNodeLink { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
