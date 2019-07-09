999deb46744fdfccabb309d5fb69984039e1630a
Lukasz Smierzchala <Lukasz.Smierzchala@gmail.com>
2019-07-09 22:24:26 +0200

    Add Mock for Sensors

---
+++ Mocks/SensorMockApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SensorMockApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
