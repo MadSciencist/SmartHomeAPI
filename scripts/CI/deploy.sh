d3474e6d12da45599c3d3096f45a5c6f27f60280
Mateusz Kryszczak <mkrysz1337@gmail.com>
2019-07-25 20:09:15 +0200

    CI #6

--- scripts/CI/deploy.sh
+++ scripts/CI/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-echo heree
-pwd
-ls > log.txt
+echo Starting deploy.sh
+
+# todo: fetch frontend release, unpack, put to wwwrooot
+dotnet publish ./src/SmartHome.API/SmartHome.API.csproj -c Release -r linux-arm
+tar -czvf smart_home_api_release_linux_arm.tar.gz ./src/SmartHome.API/bin/Release/netcoreapp2.2/linux-arm/publish/
+
+echo Done deploy.sh
